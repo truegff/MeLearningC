b4e4626c13e70b8380426cc285af47abc1407db4
Alexander Dolidze <alexander.dolidze@gmail.com>
2015-04-01 02:20:40 +0400

    finished chapter 1.3 - declaring variables

---
+++ Chapter_01/1.3/example2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	char ch;
+	float f;
+	double d;
+
+	ch = 'X';
+	f = 100.123;
+	d = 123.009;
+
+	printf("ch is %c\n", ch);
+	printf("f is %f\n", f);
+	printf("d is %f\n", d);
+
+	return 0;
+}
